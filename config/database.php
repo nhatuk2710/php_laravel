445f04c1945e6c1bd0db9761aafbb2c06427c6a7
nhatuk2710 <49880246+nhatuk2710@users.noreply.github.com>
2020-03-27 12:40:32 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -65,7 +65,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-//            'url' =>  env('DATABASE_URL'),
+            'url' =>  env('DATABASE_URL'),
             'host' => "ec2-52-73-247-67.compute-1.amazonaws.com",//env('DB_HOST', '127.0.0.1'),
             'port' => '5432',//env('DB_PORT', '5432'),
             'database' => "d63vf897b5ncoc",//env('DB_DATABASE', 'forge'),
